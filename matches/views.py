d65f0f7b18afa74319cc2b7965678ae7569aed63
praveen-aitipamula <aitipamulapraven@gmail.com>
2026-06-09 01:15:19 +0530

    NRR added to points table

--- matches/views.py
+++ matches/views.py
@@ -124,16 +124,43 @@ def PointsTableView(request):
 
         points = int(won) * 2
 
+        runs_scored = 0
+        overs_faced = 0
+        runs_conceded = 0
+        overs_bowled = 0
+
+        matches = Match.objects.filter(status="Completed").filter(Q(team1=franchise)|Q(team2=franchise))
+        for match in matches:
+            if franchise == match.team1:
+                runs_scored += int(match.team1_score)
+                overs_faced += float(match.team1_overs)
+
+                runs_conceded += int(match.team2_score)
+                overs_bowled += float(match.team2_overs)
+            else:
+                runs_scored += int(match.team2_score)
+                overs_faced += float(match.team2_overs)
+
+                runs_conceded += int(match.team1_score)
+                overs_bowled += float(match.team1_overs)
+
+            if overs_faced > 0 and overs_bowled > 0:
+                nrr= (runs_scored/overs_faced)-(runs_conceded/overs_bowled)
+            else:
+                nrr = 0
+
+
         table.append({
             "team": franchise,
             "played": played,
             "won": won,
             "lost": lost,
             "points": points,
+            "nrr":round(nrr,3)
         })
         table = sorted(
             table,
-            key=lambda x: x["points"],
+            key=lambda x: (x["points"],x["nrr"]),
             reverse=True
         )
 
