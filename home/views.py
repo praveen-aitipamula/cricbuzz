93ca0cc1b69614b9b0c916c14ec6c9cc9281ba48
praveen-aitipamula <aitipamulapraven@gmail.com>
2026-06-09 18:27:41 +0530

    home page sections added

--- home/views.py
+++ home/views.py
@@ -1,13 +1,26 @@
-from multiprocessing import context
-from operator import ge
-
 from django.shortcuts import render
 from matches.utils import get_points_table
+from matches.models import Match
+
 
 # Create your views here.
 def home(request):
+    matches = Match.objects.filter(
+        status="Completed"
+    ).order_by("-id")[:3]
+
+    upcoming_matches = Match.objects.filter(
+        status="Scheduled"
+    ).order_by("id")[:5]
+
+    
+
+
+
     table = get_points_table()
     context ={
-        "table":table
+        "matches":matches,
+        "table":table,
+        "upcoming_matches": upcoming_matches,
     }
     return render(request, "home.html",context)
